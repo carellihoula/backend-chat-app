6cea52fee2af176fcb04e2fd1be89c42df748504
carel ntsoumou <carel.ntsoumou_lihoula.etu@univ-lemans.fr>
2024-04-21 01:33:03 +0200

    refacto cors

--- src/main/java/com/carel/backendapp/security/SecurityConfig.java
+++ src/main/java/com/carel/backendapp/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://canoappchat.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT", "OPTIONS"));
         configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
