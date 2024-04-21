d245c7d717ba2cb50de7123ad68ee83dd63ed7c7
carel ntsoumou <carel.ntsoumou_lihoula.etu@univ-lemans.fr>
2024-04-21 04:20:46 +0200

    add rds

--- src/main/java/com/carel/backendapp/configWebsocket/WebSocketConfig.java
+++ src/main/java/com/carel/backendapp/configWebsocket/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("https://canoappchat.vercel.app")
                 .withSockJS();
     }
 
